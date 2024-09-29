0088945e1bbac6f448084ad64ef92736021c76bd
yuna1313 <93983333+yuna1313@users.noreply.github.com>
2024-09-29 23:31:32 +0900

    프로젝트 세팅

---
+++ src/main/java/com/record/budget/RecordBugdetApplication.java
@@ -0,0 +1,13 @@
+package com.record.budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordBugdetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordBugdetApplication.class, args);
+	}
+
+}
